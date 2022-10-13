644adf9b32010366d7a9f8be9805a3cff3fa6900
douxiaobo <dxb_1020@icloud.com>
2022-10-13 15:34:26 +0800

    remote commit

---
+++ 1001-害死人不偿命的(3n+1)猜想/c-gcc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int step=0,n;
+    (void)scanf("%d",&n);
+    while(n!=1)
+    {
+        if(n%2==1)
+            n=3*n+1;
+        else
+        {
+            n=n/2;
+            step++;
+        }
+    }
+    printf("%d",step);
+    return 0;
+}
