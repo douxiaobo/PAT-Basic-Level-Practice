514a1398f069109922a22c51ef69d54e07229b4f
douxiaobo <dxb_1020@icloud.com>
2022-10-14 15:36:10 +0800

    remote commit

---
+++ 1001-害死人不偿命的(3n+1)猜想/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int step=0;
+            int n=int.Parse(Console.ReadLine());
+            while(n!=1)
+            {
+                if(n%2==1)
+                    n=3*n+1;
+                else
+                {
+                    n=n/2;
+                    step++;
+                }
+            }
+            Console.WriteLine(step);
+        }
+    }
+}
